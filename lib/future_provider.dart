7551d3ac10d572382bcc39723ecf315c8050bf69
DipuChandraRoy <dipuray7779@gmail.com>
2026-08-28 14:23:21 +0600

    update future provider and home

--- lib/future_provider.dart
+++ lib/future_provider.dart
@@ -1,7 +1,17 @@
+// import 'package:riverpod/riverpod.dart';
+
+// final futureProvider = FutureProvider<int>((ref) async {
+//   await Future.delayed(Duration(seconds: 2));
+//   throw "Network Not Working";
+
+//   return 3;
+// });
+
 import 'package:riverpod/riverpod.dart';
 
-final futureProvider = FutureProvider<int>((ref) async {
+final futureProvider = FutureProvider<List<String>>((ref) async {
   await Future.delayed(Duration(seconds: 2));
+  //   throw "Network Not Working";
 
-  return 3;
+  return ["Emon", "Habib"];
 });
--- lib/home.dart
+++ lib/home.dart
@@ -11,10 +11,21 @@ class HomePage extends ConsumerWidget {
     return Scaffold(
       body: Center(
         child: provider.when(
-          data: (value) => Text(value.toString()),
+          skipLoadingOnRefresh: false,
+          data: (value) => ListView.builder(
+            itemCount: value.length,
+            itemBuilder: ((context, index) {
+              return Text(value[index].toString());
+            }),
+          ),
           error: (error, stack) => Text(error.toString()),
           loading: () => CircularProgressIndicator(),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.invalidate(futureProvider);
+        },
       ),
     );
   }
